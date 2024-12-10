8e80a4beab946d952905e0a55bb79d9f1387cf38
edwin chris <officialedwinchris@gmail.com>
2024-12-10 01:15:10 +0100

    fix import error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import frame1 from '../assets/frame1.png'
+import frame1 from '../assets/frame.png'
 import frame2 from '../assets/frame2.png'
 import frame3 from '../assets/frame3.png'
 import frame4 from '../assets/frame4.png'
